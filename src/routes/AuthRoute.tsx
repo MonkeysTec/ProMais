9c126b689242fef31efe8855c2c67f2f20a4daf4
RodrigoFaccio <rodrigo.facio@adminyogha.com.br>
2024-05-31 14:57:27 -0300

    tab navigator

--- src/routes/AuthRoute.tsx
+++ src/routes/AuthRoute.tsx
@@ -4,9 +4,40 @@ import { Image } from 'react-native';
 import HomeScreen from '../screens/Home';
 import News from '../screens/News';
 import Bipador from '../screens/Bipador';
+import { Ionicons } from '@expo/vector-icons';
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+const Tab = createBottomTabNavigator();
 
 const Stack = createStackNavigator();
+const TabNavigator: React.FC = () => (
+  <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
 
+        if (route.name === 'Home') {
+          iconName = 'home';
+        } else if (route.name === 'QRCode') {
+          iconName = 'camera';
+        } else if (route.name === 'Profile') {
+          iconName = 'person';
+        }
+
+        size = focused ? size + 4 : size;
+
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+      tabBarActiveTintColor: '#85d151',
+      tabBarInactiveTintColor: 'gray',
+    })}
+  >
+    <Tab.Screen name="Home" options={{ headerShown: false }}   component={HomeScreen} />
+    <Tab.Screen name="Profile" options={{ headerShown: false }}   component={HomeScreen} />
+
+
+  </Tab.Navigator>
+);
 const AuthRoutes = () => {
   return (
     <Stack.Navigator
@@ -20,7 +51,7 @@ const AuthRoutes = () => {
         
       }}
     >
-      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Home" component={TabNavigator} />
       <Stack.Screen name="TotalEnergies" component={News} />
       <Stack.Screen name="Bipador" component={Bipador} />
       <Stack.Screen name="News" component={News} />
